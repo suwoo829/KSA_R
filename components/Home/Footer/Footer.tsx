d4eb873932f93012c316464dad92deff2d6ebb32
sungjunc <sungjunc@gmail.com>
2025-03-10 04:34:33 +0800

    Fix type errors in ReviewCard and Blog components

--- components/Home/Footer/Footer.tsx
+++ components/Home/Footer/Footer.tsx
@@ -13,13 +13,6 @@ const Footer = () => {
           className="mx-auto"
         />
       </div>
-      <div className="flex items-center flex-wrap justify-center space-x-10 space-y-4 text-white font-bold">
-        <div>Home.</div>
-        <div>Services.</div>
-        <div>Project.</div>
-        <div>Reviews.</div>
-        <div>Contact</div>
-      </div>
       <p className="text-white text-opacity-60 mt-6 text-center">
         © 2024 All Rights Reserved by WebDev Warriors
       </p>
