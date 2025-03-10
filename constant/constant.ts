9884081bc1da173ef2e169cfff126a82d85781ed
sungjunc <sungjunc@gmail.com>
2025-03-10 08:25:20 +0800

    Fix type errors in ReviewCard and Blog components

--- constant/constant.ts
+++ constant/constant.ts
@@ -1,32 +1,32 @@
-// export const navLinks = [
-//   // {
-//   //   id: 1,
-//   //   url: "#Home",
-//   //   label: "Home",
-//   // },
-//   // {
-//   //   id: 5,
-//   //   url: "#About",
-//   //   label: "About",
-//   // },
-//   // {
-//   //   id: 3,
-//   //   url: "#Services",
-//   //   label: "Services",
-//   // },
-//   // {
-//   //   id: 4,
-//   //   url: "#Projects",
-//   //   label: "Projects",
-//   // },
-//   // {
-//   //   id: 2,
-//   //   url: "#Reviews",
-//   //   label: "Reviews",
-//   // },
-//   // {
-//   //   id: 6,
-//   //   url: "#Contacts",
-//   //   label: "Contacts",
-//   // },
-// ];
+export const navLinks = [
+  {
+    id: 1,
+    url: "#votingUrl",
+    label: "",
+  },
+  // {
+  //   id: 5,
+  //   url: "#About",
+  //   label: "About",
+  // },
+  // {
+  //   id: 3,
+  //   url: "#Services",
+  //   label: "Services",
+  // },
+  // {
+  //   id: 4,
+  //   url: "#Projects",
+  //   label: "Projects",
+  // },
+  // {
+  //   id: 2,
+  //   url: "#Reviews",
+  //   label: "Reviews",
+  // },
+  // {
+  //   id: 6,
+  //   url: "#Contacts",
+  //   label: "Contacts",
+  // },
+];
